70adf778682439e40028c4737112667e67d2bd64
kazuya utsunomiya <yungle800@gmail.com>
2020-03-05 22:27:42 +0900

    first Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,147 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="screen">{{ currentPageA }}</div>
+    <SwipeMenu class="buttons"
+               :loop="true"
+               :currentPage="currentPageA"
+               :itemWidth="224"
+               @move="onMoveA"
+               @touch-end="onTouchEnd"
+               @change-page="onChangePageA">
+      <div class="infinity-swipe-item" v-for="(button, index) in buttonsA" :key="index">
+        <button
+                :class="{active: currentPageA==index+1, disabled: button.disabled}"
+                @click="onButtonClickA(index+1)">
+          {{button.name}}
+        </button>
+      </div>
+    </SwipeMenu>
+
+    <div class="screen">{{ buttonsB[currentPageB-1].color }}</div>
+    <SwipeMenu class="buttons"
+               :loop="true"
+               :currentPage="currentPageB"
+               :itemWidth="50"
+               @move="onMoveB"
+               @touch-end="onTouchEnd"
+               @change-page="onChangePageB">
+      <div class="infinity-swipe-item" v-for="(button, index) in buttonsB" :key="index">
+        <button
+          class="btn-color"
+          :class="{active: currentPageB==index+1, disabled: button.disabled}"
+          @click="onButtonClickB(index+1)" :style="'background-color:'+button.color+';'">
+          {{button.color}}
+        </button>
+      </div>
+    </SwipeMenu>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import SwipeMenu from './components/SwipeMenu.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'App',
+    components: {
+      SwipeMenu,
+    },
+    data() {
+      return {
+        touchTime: 0,
+        currentPageA: 1,
+        buttonsA: [
+          {name: 'メニュー１'},
+          {name: 'メニュー２'},
+          {name: 'メニュー３'},
+          {name: 'メニュー４'},
+        ],
+        currentPageB: 1,
+        buttonsB: [],
+      };
+    },
+    created () {
+      let count = 0;
+      while (count < 50) {
+        this.buttonsB.push({
+          color: '#' +
+            ('00' + Math.floor(Math.random()*Math.floor(255)).toString(16)).slice(-2) +
+            ('00' + Math.floor(Math.random()*Math.floor(255)).toString(16)).slice(-2) +
+            ('00' + Math.floor(Math.random()*Math.floor(255)).toString(16)).slice(-2)
+        });
+        count++;
+      }
+    },
+    methods: {
+      onButtonClickA: function (page) {
+        if (this.touchTime < 300) {
+          this.onChangePageA(page);
+        }
+      },
+      onChangePageA: function (page) {
+        this.currentPageA = page;
+      },
+      onMoveA: function (page) {
+        if (page <= 0) page = 1;
+        if (page > this.buttonsB.length) page = this.buttonsA.length;
+        if (page !== this.currentPageA) {
+          this.onChangePageA(page);
+        }
+      },
+      onButtonClickB: function (page) {
+        if (this.touchTime < 300) {
+          this.onChangePageB(page);
+        }
+      },
+      onChangePageB: function (page) {
+        this.currentPageB = page;
+      },
+      onMoveB: function (page) {
+        if (page <= 0) page = 1;
+        if (page > this.buttonsB.length) page = this.buttonsB.length;
+        if (page !== this.currentPageB) {
+          this.onChangePageB(page);
+        }
+      },
+      onTouchEnd: function (page, touchTime) {
+        this.touchTime = touchTime;
+      },
+    },
+  };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .screen {
+    font-size: 30px;
+    text-align: center;
+    width: 100%;
+    height: 200px;
+    line-height: 200px;
+  }
+  .buttons {
+    /*width: 100%;*/
+    background-color: rgba(0, 0, 0, 0.5);
+    overflow: hidden;
+
+  }
+  .buttons button {
+    width: 10rem;
+    margin: .5rem 2rem;
+    height: 40px;
+    border-radius: .5rem;
+    color: rgb(128, 127, 128);
+    background-color: #f0f0f0;
+  }
+
+  .buttons button.active {
+    color: #000;
+    background-color: #fef;
+  }
+  .buttons button.btn-color {
+    width: 50px;
+    margin: 0;
+    padding: 0;
+    border: 1px solid black;
+    font-size: 10px;
+  }
 </style>
+
---
+++ src/components/SwipeMenu.vue
@@ -0,0 +1,270 @@
+<template>
+  <div class="swipe_menu"
+       :class="[{'dragging': dragging}]"
+       @touchstart="_onTouchStart"
+       @mousedown="_onTouchStart"
+       @wheel="_onWheel">
+    <div class="wrapper"
+         ref="infinitySwipeWrapper"
+         :style="styleObject"
+         @transitionend="_onTransitionEnd">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SwipeMenu',
+    props: {
+      currentPage: {
+        type: Number,
+        default: 1
+      },
+      loop: {
+        type: Boolean,
+        default: false
+      },
+      speed: {
+        type: Number,
+        default: 500
+      },
+      center: {
+        type: Boolean,
+        default: true
+      },
+      itemWidth: {
+        type: Number,
+        default: 100
+      }
+    },
+    data () {
+      return {
+        lastPage: 1,
+        translateX: 0,
+        startTranslate: 0,
+        startTime: 0,
+        delta: 0,
+        dragging: false,
+        startPos: null,
+        transitioning: false,
+        slideEls: [],
+        translateOffset: 0,
+        transitionDuration: 0,
+        centerOffset: 0,
+        bulletWidth: null,
+      };
+    },
+    computed: {
+      styleObject: function () {
+        return {
+          transform: 'translate3d(' + this.translateX + 'px, 0, 0)',
+          transitionDuration: this.transitionDuration + 'ms'
+        };
+      },
+    },
+    mounted () {
+      this.$nextTick(function () {
+        if (this.center) {
+          this.handleResize();
+          this.translateOffset = this.centerOffset;
+        }
+        this._onTouchMove = this._onTouchMove.bind(this);
+        this._onTouchEnd = this._onTouchEnd.bind(this);
+        this.slideEls = [].map.call(this.$refs.infinitySwipeWrapper.children, el => el);
+        if (this.loop) {
+          this.$nextTick(function () {
+            this._createLoop();
+            this.setPage(this.currentPage, true);
+          });
+        } else {
+          this.setPage(this.currentPage);
+        }
+      });
+    },
+    watch: {
+      currentPage: function (page) {
+        if (this.center) {
+          this.handleResize();
+          this.translateOffset = this.centerOffset;
+        }
+        this.slideEls = [].map.call(this.$refs.infinitySwipeWrapper.children, el => el);
+        this.setPage(page, false);
+      }
+    },
+    methods: {
+      next () {
+        let page = this.currentPage;
+        if (page < this.slideEls.length || this.loop) {
+          this.setPage(page + 1);
+        } else {
+          this._revert();
+        }
+      },
+      prev () {
+        let page = this.currentPage;
+        if (page > 1 || this.loop) {
+          this.setPage(page - 1);
+        } else {
+          this._revert();
+        }
+      },
+      setPage (page, noAnimation) {
+        if (!this.dragging) {
+          let self = this;
+          if (this.loop) {
+            if (this.delta === 0) {
+              this._setTranslate(self._getTranslateOfPage(this.lastPage));
+            }
+            setTimeout(function () {
+              self._setTranslate(self._getTranslateOfPage(page));
+              if (noAnimation) return;
+              self._onTransitionStart();
+            }, 0);
+          } else {
+            this._setTranslate(this._getTranslateOfPage(page));
+            if (noAnimation) return;
+            this._onTransitionStart();
+          }
+        }
+
+        if (this._isPageChanged()) this.$emit('change-page', page);
+        this.lastPage = this.currentPage;
+      },
+      _onTouchStart (e) {
+        this.startPos = this._getTouchPos(e);
+        this.delta = 0;
+        this.startTranslate = this._getTranslateOfPage(this.currentPage);
+        this.startTime = new Date().getTime();
+        this.dragging = true;
+        this.transitionDuration = 0;
+        document.addEventListener('touchmove', this._onTouchMove, {passive: false});
+        document.addEventListener('touchend', this._onTouchEnd, false);
+        document.addEventListener('mousemove', this._onTouchMove, false);
+        document.addEventListener('mouseup', this._onTouchEnd, false);
+      },
+      _onTouchMove (e) {
+        this.delta = this._getTouchPos(e) - this.startPos;
+        this._setTranslate(this.startTranslate + this.delta);
+
+        const translate = Math.abs(this._getTranslate() - this.translateOffset);
+        const page = Math.ceil((translate + this.itemWidth / 2) / this.itemWidth);
+
+        if (page !== this.currentPage) {
+          this.setPage(page);
+        }
+
+        this.$emit('move', page);
+        if (Math.abs(this.delta) > 0) {
+          e.preventDefault();
+        }
+      },
+      _onTouchEnd () {
+        this.dragging = false;
+        this.transitionDuration = this.speed;
+        this.setPage(this.currentPage);
+
+        document.removeEventListener('touchmove', this._onTouchMove);
+        document.removeEventListener('touchend', this._onTouchEnd);
+        document.removeEventListener('mousemove', this._onTouchMove);
+        document.removeEventListener('mouseup', this._onTouchEnd);
+        this.$emit('touch-end', this.currentPage, new Date().getTime() - this.startTime);
+      },
+      _onWheel () {
+      },
+      _revert () {
+        this.setPage(this.currentPage);
+      },
+      _getTouchPos (e) {
+        let key = 'pageX';
+        return e.changedTouches ? e.changedTouches[0][key] : e[key];
+      },
+      _onTransitionStart () {
+        this.transitioning = true;
+        this.transitionDuration = this.speed;
+        if (this._isPageChanged()) {
+          this.$emit('change-start', this.currentPage);
+        } else {
+          this.$emit('revert-start', this.currentPage);
+        }
+      },
+      _onTransitionEnd () {
+        this.transitioning = false;
+        this.transitionDuration = 0;
+        this.delta = 0;
+        if (this._isPageChanged()) {
+          this.$emit('change-end', this.currentPage);
+        } else {
+          this.$emit('revert-end', this.currentPage);
+        }
+      },
+      _isPageChanged () {
+        return this.lastPage !== this.currentPage;
+      },
+      _setTranslate (value) {
+        let translateName = 'translateX';
+        this[translateName] = value;
+      },
+      _getTranslate () {
+        let translateName = 'translateX';
+        return this[translateName];
+      },
+      _getTranslateOfPage (page) {
+        if (page === 0) return 0;
+        let propName = 'clientWidth';
+        return -[].reduce.call(this.slideEls, function (total, el, i) {
+          return i > page - 2 ? total : total + el[propName];
+        }, 0) + this.translateOffset;
+      },
+      _createLoop () {
+          // @todo
+        //        this.swipe.buttons = this.swipe.buttons.concat(this.swipe.buttons);
+      },
+      handleResize () {
+        if (!this.bulletWidth) {
+          this.bulletWidth = 160;
+          if (this.$refs.infinitySwipeWrapper.children[0]) {
+            this.bulletWidth = this.$refs.infinitySwipeWrapper.children[0].clientWidth;
+          }
+        }
+        let currentCenterOffset = Math.round(this.$el.clientWidth / 2 - (this.bulletWidth / 2));
+        if (this.centerOffset !== currentCenterOffset) {
+          this.centerOffset = currentCenterOffset;
+          this.translateOffset = this.centerOffset;
+          this.setPage(this.currentPage);
+        }
+        if (this.center) {
+          if (window.requestAnimationFrame) {
+            window.requestAnimationFrame(() => {
+              this.handleResize();
+            });
+          } else {
+            setTimeout(() => {
+              this.handleResize();
+            }, 1000);
+          }
+        }
+      }
+    },
+    serverCacheKey: props => props.show
+  };
+</script>
+
+<style scoped>
+  .swipe_menu {
+    margin: 0 auto;
+    position: relative;
+    overflow: hidden;
+    z-index: 1;
+  }
+  .wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    display: flex;
+    -webkit-transition-property: -webkit-transform;
+    transition-property: transform;
+    box-sizing: content-box;
+  }
+</style>
